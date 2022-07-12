f25f77f30442f5a54492cd0a7d93f38671f4bfa9
Yoo <edel1212@naver.com>
2022-07-12 22:57:44 +0900

    파일 수정 스크립트 끝

--- src/main/webapp/resources/js/board/get.js
+++ src/main/webapp/resources/js/board/get.js
@@ -111,7 +111,7 @@ class Board {
          */
         if (this.modify instanceof HTMLButtonElement) {
             this.modify.addEventListener("click", () => {
-                var _a;
+                var _a, _b;
                 if (!this.modiBtnChang) {
                     const modifyFlag = confirm(`해당 게시물을 수정 하시겠습니까?`);
                     if (modifyFlag) {
@@ -132,6 +132,108 @@ class Board {
                 <input id="fileInput" type="file" name="uploadFile" multiple >
               </div>`);
                         /***@TODO : 등록 event 만들어주자 this.files에 정보를 put 해줘야함! */
+                        (_a = document
+                            .querySelector("#fileInput")) === null || _a === void 0 ? void 0 : _a.addEventListener("change", (e) => {
+                            /** 데이터를 담을 객체 */
+                            let formData = new FormData();
+                            /** target Input */
+                            let inputFile = document.querySelector("input[name='uploadFile']");
+                            if (inputFile instanceof HTMLInputElement) {
+                                /** 배열 형태로 데이터를 나열 */
+                                const files = inputFile.files;
+                                for (let i of files) {
+                                    //검사
+                                    if (!checkExtension2(i.name, i.size))
+                                        return;
+                                    //FormData 객체에 파일을 주입
+                                    formData.append("uploadFile", i);
+                                } //for
+                                for (let key of formData.keys()) {
+                                    console.log(key);
+                                }
+                                for (let value of formData.values()) {
+                                    console.log(value);
+                                }
+                                fetch("/board/uploadAction", {
+                                    method: "POST",
+                                    cache: "no-cache",
+                                    body: formData,
+                                })
+                                    .then((response) => response.json())
+                                    .then((data) => {
+                                    console.log("data", data);
+                                    //file input 초기화
+                                    const fileInput = document.querySelector("input[name='uploadFile']");
+                                    if (fileInput instanceof HTMLInputElement) {
+                                        fileInput.value = "";
+                                    }
+                                    //파일 목록 생성
+                                    const uploadRstUL = document.querySelector(".uploadResult ul");
+                                    let str = "";
+                                    data.forEach((obj) => {
+                                        const FileDownCallPath = encodeURIComponent(obj["uploadPath"] +
+                                            "/" +
+                                            obj["uuid"] +
+                                            "_" +
+                                            obj["fileName"]);
+                                        if (!obj["image"]) {
+                                            obj.fileType = false;
+                                            str +=
+                                                "<li style='display:flex'><img style='width:25px;margin-right:5px;' src='/resources/img/file.png'>";
+                                            str += "<a href='";
+                                            str += "/board/download?fileName=" + FileDownCallPath;
+                                            str += "'>";
+                                            str += obj["fileName"];
+                                            str += "</a>";
+                                            str +=
+                                                "<button type='button' style='background: none;border: none;color: red;' data-file=";
+                                            str +=
+                                                FileDownCallPath +
+                                                    " data-type='file' " +
+                                                    "data-uuid=" +
+                                                    obj["uuid"];
+                                            str += ">X</button>";
+                                            str += "</li>";
+                                        }
+                                        else {
+                                            obj.fileType = true;
+                                            const thumFileCallPath = encodeURIComponent(obj["uploadPath"] +
+                                                "/" +
+                                                "s_" +
+                                                obj["uuid"] +
+                                                "_" +
+                                                obj["fileName"]);
+                                            str += "<li style='display:flex'>";
+                                            str +=
+                                                "<img style='width:25px;margin-right:5px;' src='/board/display?fileName=" +
+                                                    thumFileCallPath +
+                                                    "'>";
+                                            str += "<a href='";
+                                            str += "/board/download?fileName=" + FileDownCallPath;
+                                            str += "'>";
+                                            str += obj["fileName"];
+                                            str += "</a>";
+                                            str +=
+                                                "<button type='button' style='background: none;border: none;color: red;' data-file=";
+                                            str +=
+                                                thumFileCallPath +
+                                                    " data-type='image' " +
+                                                    "data-uuid=" +
+                                                    obj["uuid"];
+                                            str += ">X</button>";
+                                            str += "</li>";
+                                        } //if-else
+                                        /**전역변수에 파일 등록 */
+                                        this.files.push(obj);
+                                    });
+                                    console.log("register files ::", this.files);
+                                    if (uploadRstUL instanceof HTMLUListElement) {
+                                        uploadRstUL.insertAdjacentHTML("beforeend", str);
+                                    }
+                                })
+                                    .catch((error) => console.log(error));
+                            }
+                        });
                         //삭제버튼 활성화
                         document
                             .querySelectorAll(".uploadResult ul li button")
@@ -141,8 +243,8 @@ class Board {
                             }
                         });
                         //삭제 버튼 Event
-                        (_a = document
-                            .querySelector(".uploadResult ul")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (e) => {
+                        (_b = document
+                            .querySelector(".uploadResult ul")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", (e) => {
                             const target = e.target;
                             if (target.nodeName !== "BUTTON") {
                                 return;
@@ -389,4 +491,21 @@ class Reply {
  *                /board/list로 변경하는 방법을 사용함
  */
 history.replaceState(null, "goList", "/board/list");
+/***File Upload 처리 */
+//확장자 정규식
+const regex2 = new RegExp("(.*?).(exe|shzip|alz)$");
+//5MB
+const maxSize2 = 524880;
+//허용 검사
+const checkExtension2 = (fileName, fileSize) => {
+    if (fileSize >= maxSize2) {
+        alert("파일사이즈 초과");
+        return false;
+    }
+    if (regex2.test(fileName)) {
+        alert("해당 종류의 파일은 업로드할 수 없습니다.");
+        return false;
+    }
+    return true;
+};
 //# sourceMappingURL=get.js.map
--- src/main/webapp/resources/ts/board/get.ts
+++ src/main/webapp/resources/ts/board/get.ts
@@ -158,6 +158,122 @@ class Board {
             );
 
             /***@TODO : 등록 event 만들어주자 this.files에 정보를 put 해줘야함! */
+            document
+              .querySelector("#fileInput")
+              ?.addEventListener("change", (e) => {
+                /** 데이터를 담을 객체 */
+                let formData = new FormData();
+                /** target Input */
+                let inputFile = document.querySelector(
+                  "input[name='uploadFile']"
+                );
+
+                if (inputFile instanceof HTMLInputElement) {
+                  /** 배열 형태로 데이터를 나열 */
+                  const files = inputFile.files as any;
+
+                  for (let i of files) {
+                    //검사
+                    if (!checkExtension2(i.name, i.size)) return;
+                    //FormData 객체에 파일을 주입
+                    formData.append("uploadFile", i);
+                  } //for
+
+                  for (let key of formData.keys()) {
+                    console.log(key);
+                  }
+                  for (let value of formData.values()) {
+                    console.log(value);
+                  }
+
+                  fetch("/board/uploadAction", {
+                    method: "POST",
+                    cache: "no-cache",
+                    body: formData,
+                  })
+                    .then((response) => response.json())
+                    .then((data) => {
+                      console.log("data", data);
+                      //file input 초기화
+                      const fileInput = document.querySelector(
+                        "input[name='uploadFile']"
+                      );
+                      if (fileInput instanceof HTMLInputElement) {
+                        fileInput.value = "";
+                      }
+
+                      //파일 목록 생성
+                      const uploadRstUL =
+                        document.querySelector(".uploadResult ul");
+                      let str = "";
+                      data.forEach((obj: AttachObj) => {
+                        const FileDownCallPath = encodeURIComponent(
+                          obj["uploadPath"] +
+                            "/" +
+                            obj["uuid"] +
+                            "_" +
+                            obj["fileName"]
+                        );
+
+                        if (!obj["image"]) {
+                          obj.fileType = false;
+                          str +=
+                            "<li style='display:flex'><img style='width:25px;margin-right:5px;' src='/resources/img/file.png'>";
+                          str += "<a href='";
+                          str += "/board/download?fileName=" + FileDownCallPath;
+                          str += "'>";
+                          str += obj["fileName"];
+                          str += "</a>";
+                          str +=
+                            "<button type='button' style='background: none;border: none;color: red;' data-file=";
+                          str +=
+                            FileDownCallPath +
+                            " data-type='file' " +
+                            "data-uuid=" +
+                            obj["uuid"];
+                          str += ">X</button>";
+                          str += "</li>";
+                        } else {
+                          obj.fileType = true;
+                          const thumFileCallPath = encodeURIComponent(
+                            obj["uploadPath"] +
+                              "/" +
+                              "s_" +
+                              obj["uuid"] +
+                              "_" +
+                              obj["fileName"]
+                          );
+                          str += "<li style='display:flex'>";
+                          str +=
+                            "<img style='width:25px;margin-right:5px;' src='/board/display?fileName=" +
+                            thumFileCallPath +
+                            "'>";
+                          str += "<a href='";
+                          str += "/board/download?fileName=" + FileDownCallPath;
+                          str += "'>";
+                          str += obj["fileName"];
+                          str += "</a>";
+                          str +=
+                            "<button type='button' style='background: none;border: none;color: red;' data-file=";
+                          str +=
+                            thumFileCallPath +
+                            " data-type='image' " +
+                            "data-uuid=" +
+                            obj["uuid"];
+                          str += ">X</button>";
+                          str += "</li>";
+                        } //if-else
+                        /**전역변수에 파일 등록 */
+                        this.files.push(obj);
+                      });
+                      console.log("register files ::", this.files);
+                      if (uploadRstUL instanceof HTMLUListElement) {
+                        uploadRstUL.insertAdjacentHTML("beforeend", str);
+                      }
+                    })
+                    .catch((error) => console.log(error));
+                }
+              });
 
             //삭제버튼 활성화
             document
@@ -413,3 +529,22 @@ class Reply {
  *                /board/list로 변경하는 방법을 사용함
  */
 history.replaceState(null, "goList", "/board/list");
+
+/***File Upload 처리 */
+//확장자 정규식
+const regex2 = new RegExp("(.*?).(exe|shzip|alz)$");
+//5MB
+const maxSize2 = 524880;
+
+//허용 검사
+const checkExtension2 = (fileName: string, fileSize: number) => {
+  if (fileSize >= maxSize2) {
+    alert("파일사이즈 초과");
+    return false;
+  }
+  if (regex2.test(fileName)) {
+    alert("해당 종류의 파일은 업로드할 수 없습니다.");
+    return false;
+  }
+  return true;
+};
